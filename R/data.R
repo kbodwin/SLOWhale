b7cb3b3f878bc7e87498ac0d5a469e090c5eb84d
Kelly N. Bodwin <kelly@bodwin.us>
2025-06-02 16:39:03 -0700

    update docs

--- R/data.R
+++ R/data.R
@@ -1,4 +1,6 @@
-#' Pretrained model artifacts
+#' Pretrained model
+#' Random forest on Melfcc-transformed data
+#' Butchered with `butcher` package to make smaller.
 #'
-#' @format A data frame.
+#' @format A pretrained random forest tidymodels object.
 "rf_melfcc_small"
